677402fb924f390073e3c8c92667fcc74fbe538e
marijapuskaric <marija.puskaric157@gmail.com>
2024-02-26 16:27:33 +0100

    Added read functions for tables StudyProgramme, AcademicYear, MaturaLevel and MaturaGrade, created form for those fields.

---
+++ EnglishLevelAssessment/Services/MaturaService.cs
@@ -0,0 +1,27 @@
+﻿using EnglishLevelAssessment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishLevelAssessment.Services
+{
+    public class MaturaService
+    {
+        private EnglishLevelAssessmentContext _context;
+
+        public MaturaService(EnglishLevelAssessmentContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MaturaLevel>> GetMaturaLevels()
+        {
+            var list = await _context.MaturaLevels.AsNoTracking().ToListAsync();
+            return list;
+        }
+
+        public async Task<List<MaturaGrade>> GetMaturaGrades()
+        {
+            var list = await _context.MaturaGrades.AsNoTracking().ToListAsync();
+            return list;
+        }
+    }
+}
