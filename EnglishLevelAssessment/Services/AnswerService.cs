1fad5b9ba91a8c627b77230ef25ceb0deb67a502
marijapuskaric <marija.puskaric157@gmail.com>
2024-03-08 21:55:06 +0100

    Fixed question-answer display, saved answers and calculated results (%)

--- EnglishLevelAssessment/Services/AnswerService.cs
+++ EnglishLevelAssessment/Services/AnswerService.cs
@@ -17,5 +17,20 @@ namespace EnglishLevelAssessment.Services
             var list = await _context.Answers.Where(p => p.QuestionId == questionId).AsNoTracking().ToListAsync();
             return list;
         }
+
+        public async Task<Answer> GetAnswerById(int id)
+        {
+            var answer = await _context.Answers
+                        .Select(p => new Answer
+                        {
+                            Id = p.Id,
+                            Answer1 = p.Answer1,
+                            QuestionId = p.QuestionId,
+                            IsCorrect = p.IsCorrect,
+                            CreatedAt = p.CreatedAt,
+                            IsDeleted = p.IsDeleted
+                        }).Where(p => p.Id == id).FirstOrDefaultAsync();
+            return answer;
+        }
     }
 }
